c06e010bc8eb0059b0431e8260f6a57322111dea
chen <23054115+cwkang1998@users.noreply.github.com>
2024-03-17 08:48:42 +0800

    chore: setup address config

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+const config = {
+  bananaControllerAddress: {
+    // Arbitrum sepolia
+    421614: "0x0e1E9dEcd77F96Ed2d6d61c36aC658404c5a3423",
+    // Base sepolia
+    84532: "0xae37D5b26C4eD804516261041BD332aC6eC7Ff27",
+  },
+};
+
+export default config;
