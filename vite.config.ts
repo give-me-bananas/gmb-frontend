6c72f5f341e9aff5f89ab62fa019ef8e7db813a8
chen <23054115+cwkang1998@users.noreply.github.com>
2024-06-13 07:35:29 +0800

    chore: init shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env,
+    "process.env": process.env,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   plugins: [react()],
-})
+});
