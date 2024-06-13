e5e22dffec9e15386e5f03a86156e1fae79ac5b8
chen <23054115+cwkang1998@users.noreply.github.com>
2024-06-13 14:41:04 +0800

    chore: add sepolia configurations

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,16 @@ const config = {
     421614: "arbitrum-sepolia",
     // Base sepolia
     84532: "base-sepolia",
+    // Sepolia
+    11155111: "sepolia",
   },
   bananaControllerAddress: {
     // Arbitrum sepolia
     421614: "0x0e1E9dEcd77F96Ed2d6d61c36aC658404c5a3423",
     // Base sepolia
     84532: "0xae37D5b26C4eD804516261041BD332aC6eC7Ff27",
+    // Sepolia
+    11155111: "0x89f378972c4173595e52C07A842C2D6F6CA5024B",
   },
   apeTokenAddress: {
     // Base sepolia
@@ -20,6 +24,8 @@ const config = {
     421614: "0x0e1E9dEcd77F96Ed2d6d61c36aC658404c5a3423",
     // Base sepolia
     84532: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+    // Sepolia
+    11155111: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
   },
 };
 
